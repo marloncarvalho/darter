af8a61dda3ddd9830f8ec0a0d5d2e840becda242
Marlon Carvalho <marlon.carvalho@gmail.com>
2015-07-16 11:19:55 -0300

    This fixes #11, fixes #12.

--- lib/src/path.dart
+++ lib/src/path.dart
@@ -1,10 +1,10 @@
 library darter.http.path;
 
 /**
- * Turns it easy to:
+ * Its main goals is to turn it easy to:
  * 1. compare whether two paths are equal or not.
- * 2. get subpaths from paths.
- * 3. handle path parts.
+ *  2. get subpaths from paths.
+ *  3. efficiently handle path parts.
  */
 class Path {
   List<String> parts = [];
@@ -12,17 +12,25 @@ class Path {
   Path() {
   }
 
+  /**
+   * Create a new instance from a String that represents a URI.
+   */
   Path.fromString(String path) {
-    for (String part in path.split("/")) {
-      if (part != null && !part.isEmpty) parts.add(part);
+    if (path != null) {
+      for (String part in path.split("/")) {
+        if (part != null && !part.isEmpty) parts.add(part);
+      }
     }
   }
 
+  /**
+   * Create a child path from this one.
+   */
   Path subPath({int start, int end}) {
     Path result = null;
 
     if (start != null) {
-      if(parts.getRange(start, end).length > 0){
+      if (parts.getRange(start, end).length > 0) {
         result = new Path();
         parts.getRange(start, end).forEach((String p) => result.parts.add(p));
       }
@@ -31,28 +39,21 @@ class Path {
     return result;
   }
 
+  /**
+   * How many parts this path is composed of?
+   */
   int get length {
     return parts.length;
   }
 
-  String getPathParams(String url) {
-
-  }
-
-  bool hasPathParam() {
-    bool result = false;
-
-    for (var i = 0;i < parts.length; i++) {
-      if (parts[i].indexOf(":") > 0) {
-        result = true;
-        break;
-      }
+  /**
+   * Join two paths.
+   */
+  Path join(Path path) {
+    if(path == null) {
+      throw new ArgumentError();
     }
 
-    return result;
-  }
-
-  Path join(Path path) {
     Path result = new Path();
     parts.forEach((p) => result.parts.add(p));
     path.parts.forEach((p) => result.parts.add(p));
